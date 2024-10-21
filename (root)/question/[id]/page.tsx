357fe764493cfccf5a1a0f8cee7315eea5105bc8
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2024-10-21 19:44:57 +0500

    votes

--- (root)/question/[id]/page.tsx
+++ (root)/question/[id]/page.tsx
@@ -1,83 +1,83 @@
-import Answer from '@/components/forms/Answer'
-import AllAnswers from '@/components/shared/AllAnswers'
-import Metric from '@/components/shared/Metric'
-import ParseHTML from '@/components/shared/ParseHTML'
-import RenderTag from '@/components/shared/RenderTag'
-import Votes from '@/components/shared/Votes'
-import { getQuestionById } from '@/lib/actions/question.action'
-import { getUserById } from '@/lib/actions/user.action'
-import { formatAndDivideNumber, getTimestamp } from '@/lib/utils'
-import { auth } from '@clerk/nextjs/server'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Answer from "@/components/forms/Answer";
+import AllAnswers from "@/components/shared/AllAnswers";
+import Metric from "@/components/shared/Metric";
+import ParseHTML from "@/components/shared/ParseHTML";
+import RenderTag from "@/components/shared/RenderTag";
+import Votes from "@/components/shared/Votes";
+import { getQuestionById } from "@/lib/actions/question.action";
+import { getUserById } from "@/lib/actions/user.action";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
+import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Page = async ({ params, searchParams }) => {
-  const { userId: clerkId } = auth()
+  const { userId: clerkId } = auth();
 
-  let mongoUser
+  let mongoUser;
 
   if (clerkId) {
-    mongoUser = await getUserById({ userId: clerkId })
+    mongoUser = await getUserById({ userId: clerkId });
   }
 
-  const result = await getQuestionById({ questionId: params.id })
+  const result = await getQuestionById({ questionId: params.id });
 
   return (
     <>
-      <div className='flex-start w-full flex-col'>
-        <div className='flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2'>
+      <div className="flex-start w-full flex-col">
+        <div className="flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
           <Link
             href={`/profile/${result.author?.clerkId}`}
-            className='flex items-center justify-start gap-1'
+            className="flex items-center justify-start gap-1"
           >
             <Image
               src={result.author?.picture}
-              className='rounded-full'
+              className="rounded-full"
               width={22}
               height={22}
-              alt='profile'
+              alt="profile"
             />
-            <p className='paragraph-semibold text-dark300_light700'>
+            <p className="paragraph-semibold text-dark300_light700">
               {result.author?.name}
             </p>
           </Link>
-          <div className='flex justify-end'>
+          <div className="flex justify-end">
             <Votes />
           </div>
         </div>
-        <h2 className='h2-semibold text-dark200_light900 mt-3.5 w-full text-left'>
+        <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
           {result.title}
         </h2>
       </div>
 
-      <div className='mb-8 mt-5 flex flex-wrap gap-4'>
+      <div className="mb-8 mt-5 flex flex-wrap gap-4">
         <Metric
-          imgUrl='/assets/icons/clock.svg'
-          alt='clock icon'
+          imgUrl="/assets/icons/clock.svg"
+          alt="clock icon"
           value={` asked ${getTimestamp(result.createdAt)}`}
-          title=' Asked'
-          textStyles='small-medium text-dark400_light800'
+          title=" Asked"
+          textStyles="small-medium text-dark400_light800"
         />
         <Metric
-          imgUrl='/assets/icons/message.svg'
-          alt='message'
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
           value={formatAndDivideNumber(result.answers?.length)}
-          title=' Answers'
-          textStyles='small-medium text-dark400_light800'
+          title=" Answers"
+          textStyles="small-medium text-dark400_light800"
         />
         <Metric
-          imgUrl='/assets/icons/eye.svg'
-          alt='eye'
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
           value={formatAndDivideNumber(result.views)}
-          title=' Views'
-          textStyles='small-medium text-dark400_light800'
+          title=" Views"
+          textStyles="small-medium text-dark400_light800"
         />
       </div>
 
       <ParseHTML data={result.content} />
 
-      <div className='mt-8 flex flex-wrap gap-2'>
+      <div className="mt-8 flex flex-wrap gap-2">
         {result.tags.map((tag: any) => (
           <RenderTag
             key={tag._id}
@@ -100,7 +100,7 @@ const Page = async ({ params, searchParams }) => {
         authorId={JSON.stringify(mongoUser?._id)}
       />
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
