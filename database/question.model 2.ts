985f5d139254513bdf3ff111cdc64fc296906d4b
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2024-10-21 19:44:20 +0500

    Answers

---
+++ database/question.model 2.ts
@@ -0,0 +1,59 @@
+import { Schema, models, model, Document } from "mongoose";
+
+export interface IQuestion extends Document {
+  title: string;
+  content: string;
+  tags: Schema.Types.ObjectId[];
+  views: number;
+  upvoted: Schema.Types.ObjectId[];
+  downvoted: Schema.Types.ObjectId[];
+  author: Schema.Types.ObjectId;
+  answers: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
+
+const QuestionSchema = new Schema<IQuestion>({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  tags: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Tag",
+    },
+  ],
+  views: {
+    type: Number,
+    default: 0,
+  },
+  upvoted: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  downvoted: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "Answer",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Question =
+  models?.Question || models?.Question || model("Question", QuestionSchema);
+
+export default Question;
